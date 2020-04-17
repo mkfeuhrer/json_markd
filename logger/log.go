eacca628359d624a489ef2cb53e7438d78ecaf5b
mkfeuhrer <mohitfeuhrer@gmail.com>
2020-04-17 18:38:23 +0530

    Add logger package for log support

---
+++ logger/log.go
@@ -0,0 +1,51 @@
+package logger
+
+import (
+	"context"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+type key int
+
+const (
+	logTracerKey key = iota
+)
+
+type Logger struct {
+	*logrus.Logger
+}
+
+var Log *Logger
+
+func panicIfError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func SetupLogger() {
+	level, err := logrus.ParseLevel("DEBUG")
+	panicIfError(err)
+
+	logrusVar := &logrus.Logger{
+		Out:       os.Stderr,
+		Formatter: &logrus.JSONFormatter{},
+		Hooks:     make(logrus.LevelHooks),
+		Level:     level,
+	}
+
+	Log = &Logger{logrusVar}
+}
+
+func WithContext(ctx context.Context, entry *logrus.Entry) context.Context {
+	return context.WithValue(ctx, logTracerKey, entry)
+}
+
+func FromContext(ctx context.Context) *logrus.Entry {
+	if log, ok := ctx.Value(logTracerKey).(*logrus.Entry); ok {
+		return log
+	}
+	return logrus.NewEntry(Log.Logger)
+}
